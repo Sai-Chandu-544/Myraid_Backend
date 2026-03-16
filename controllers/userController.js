aa6f885811b93f9961ce2e216b4411bf87347a89
Sai-Chandu544 <pendotasaichandu1234@gmail.com>
2026-03-16 10:42:21 +0530

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -53,12 +53,11 @@ exports.login = async (req, res) => {
   }
 
   const token = generateToken(user._id);
-
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict"
-  });
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,      // required for HTTPS (Vercel + Render)
+  sameSite: "none"   // required for cross-domain requests
+});
 
   res.json({
     message: "Login successful"
